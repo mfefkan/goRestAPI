a56eb7c6158c9c577d543ca1c2f9cabb4f667354
Muhammet Efkan <126757048+mfefkan@users.noreply.github.com>
2024-03-28 00:02:02 +0300

    Handler eklendi

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"test/db"
+	"test/user"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
@@ -11,6 +15,17 @@ func main() {
 		fmt.Println("Database connection error:", err)
 	}
 
-	fmt.Println(database.Config)
+	repo := user.NewRepository(database)
+	err = repo.Migration()
+	if err != nil {
+		log.Fatal(err)
+	}
+	service := user.NewService(repo)
+	handler := user.NewHandler(service)
 
+	app := fiber.New()
+	app.Get("/users/:id", handler.Get)
+	app.Post("/users", handler.Create)
+
+	app.Listen(":8000")
 }
--- user/handler.go
+++ user/handler.go
@@ -1,1 +1,58 @@
 package user
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Handler interface {
+	Get(*fiber.Ctx) error
+	Create(*fiber.Ctx) error
+}
+
+type handler struct {
+	service Service
+}
+
+var _ Handler = handler{}
+
+func NewHandler(service Service) Handler {
+	return handler{service: service}
+}
+
+type Response struct {
+	Error string      `json:"error"`
+	Data  interface{} `json:"data"`
+}
+
+func (h handler) Get(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(400).JSON(Response{Error: err.Error()})
+	}
+
+	model, err := h.service.Get(uint(id))
+	if err != nil {
+		return c.Status(400).JSON(Response{Error: err.Error()})
+	}
+
+	return c.Status(200).JSON(Response{Data: model})
+
+}
+
+func (h handler) Create(c *fiber.Ctx) error {
+	model := Model{}
+
+	err := c.BodyParser(&model)
+	if err != nil {
+		return c.Status(400).JSON(Response{Error: err.Error()})
+	}
+
+	_, err = h.service.Create(model)
+	if err != nil {
+		return c.Status(400).JSON(Response{Error: err.Error()})
+	}
+
+	return c.SendStatus(201)
+}
